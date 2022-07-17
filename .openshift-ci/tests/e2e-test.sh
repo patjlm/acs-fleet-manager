11c6a4dc2358db746dcd48e93b645a12fef29da3
Daniel Haus <dhaus@redhat.com>
2022-07-17 23:42:41 +0200

    Rebase e2e script, add missing OCM binary to dockerfile.

--- .openshift-ci/tests/e2e-test.sh
+++ .openshift-ci/tests/e2e-test.sh
@@ -1,9 +1,32 @@
 #!/usr/bin/env bash
 
+## This is the main entry point for OpenShift CI. This can also be executed locally against Minikube.
+## This script also takes care of OpenShift CI specific initialization
+## (e.g. injecting Vault secrets into the environment), spawning loggers, dumping logs at the end.
+
 GITROOT="$(git rev-parse --show-toplevel)"
 export GITROOT
 # shellcheck source=/dev/null
 source "${GITROOT}/dev/env/scripts/lib.sh"
+
+if [[ "${OPENSHIFT_CI:-}" == "true" ]]; then
+    # We are running in an OpenShift CI context, configure accordingly.
+    log "Executing in OpenShift CI context"
+    log "Retrieving secrets from Vault mount"
+    shopt -s nullglob
+    for cred in /var/run/rhacs-ms-e2e-tests/[A-Z]*; do
+        secret_name="$(basename "$cred")"
+        secret_value="$(cat "$cred")"
+        log "Got secret ${secret_name}"
+        export "${secret_name}"="${secret_value}"
+    done
+    export STATIC_TOKEN="${FLEET_STATIC_TOKEN:-}"
+    export QUAY_USER="${IMAGE_PUSH_USERNAME:-}"
+    export QUAY_TOKEN="${IMAGE_PUSH_PASSWORD:-}"
+    export CLUSTER_TYPE="openshift-ci"
+    export GOARGS="-mod=mod" # For some reason we need this in the offical base images.
+fi
+
 init
 
 bootstrap.sh
@@ -15,35 +38,147 @@ fi
 
 up.sh
 
-log "Environment up and running"
-log "Waiting for fleet-manager to complete leader election..."
-# Don't have a better way yet to wait until fleet-manager has completed the leader election.
-$KUBECTL -n "$ACSMS_NAMESPACE" logs -l application=fleet-manager -c fleet-manager -f |
-    grep -q --line-buffered --max-count=1 'Running as the leader and starting' || true
-sleep 1
+log "Cluster type: ${CLUSTER_TYPE}"
+log "Cluster name: ${CLUSTER_NAME}"
+log "Image: ${FLEET_MANAGER_IMAGE}"
+if [[ "$SPAWN_LOGGER" == "true" ]]; then
+    LOG_DIR=$(mktemp -d)
+    export LOG_DIR
+    log "Log directory: ${LOG_DIR}"
+fi
+
+if [[ -n "$OPENSHIFT_CI" ]]; then
+    log "Test suite is running in OpenShift CI"
+    export GOARGS="-mod=mod" # For some reason we need this in the offical base images.
+
+    # When running in OpenShift CI, ensure we also run the auth E2E tests.
+    RUN_AUTH_E2E="true"
+    export RUN_AUTH_E2E
+fi
+
+# If auth E2E tests shall be run, ensure we have all authentication related secrets correctly set up.
+if [[ "$RUN_AUTH_E2E" == "true" ]]; then
+    log "Setting up authentication related environment variables for auth E2E tests"
+    # FLEET_STATIC_TOKEN is the name of the secret in Vault,
+    # STATIC_TOKEN is the name expected by the application (when running directly),
+    # hence we support both names here.
+    FLEET_STATIC_TOKEN=${FLEET_STATIC_TOKEN:-}
+    export STATIC_TOKEN=${STATIC_TOKEN:-$FLEET_STATIC_TOKEN}
+
+    # Ensure we set the OCM refresh token once more, in case AUTH_TYPE!=OCM.
+    OCM_SERVICE_TOKEN=$(ocm token --refresh)
+    export OCM_SERVICE_TOKEN
+
+    # The RH SSO secrets are correctly set up within vault, the tests will be skipped if they are empty.
+fi
+
+case "$AUTH_TYPE" in
+OCM)
+
+    log "Refreshing OCM Service Token"
+    OCM_SERVICE_TOKEN=$(ocm token --refresh)
+    export OCM_SERVICE_TOKEN
+    ;;
+
+STATIC_TOKEN)
+    if [[ -z "$STATIC_TOKEN" ]]; then
+        die "Error: No static token found in the environment.\nPlease set the environment variable STATIC_TOKEN to a valid static token."
+    fi
+    log "Found static token in the environment"
+    ;;
+esac
+
+log
+
+if [[ "$INHERIT_IMAGEPULLSECRETS" == "true" ]]; then
+    if [[ -z "${QUAY_USER:-}" ]]; then
+        die "QUAY_USER needs to be set"
+    fi
+    if [[ -z "${QUAY_TOKEN:-}" ]]; then
+        die "QUAY_TOKEN needs to be set"
+    fi
+fi
+
+# Configuration specific to this e2e test suite:
+export ENABLE_DB_PORT_FORWARDING="false"
+
+bootstrap.sh
+
+if [[ "$CLUSTER_TYPE" != "openshift-ci" ]]; then
+    log "Cleaning up left-over resource (if any)"
+    down.sh 2>/dev/null
+fi
+
+MAIN_LOG="log.txt"
+
+if [[ "$SPAWN_LOGGER" == "true" ]]; then
+    log "Spawning logger, log directory is ${LOG_DIR}"
+    stern -n "$ACSMS_NAMESPACE" '.*' --color=never --template '[{{.ContainerName}}] {{.Message}}{{"\n"}}' >"${LOG_DIR}/${MAIN_LOG}" 2>&1 &
+fi
 
 FAIL=0
-if [[ "$SKIP_TESTS" == "true" ]]; then
-    log "Skipping tests"
+if ! "${GITROOT}/.openshift-ci/tests/e2e-test.sh"; then
+    FAIL=1
+fi
+
+if [[ "$DUMP_LOGS" == "true" ]]; then
+    if [[ "$SPAWN_LOGGER" == "true" ]]; then
+        log
+        log "** BEGIN LOGS **"
+        log
+
+        shopt -s nullglob
+        for logfile in "${LOG_DIR}"/*; do
+            logfile_basename=$(basename "$logfile")
+            log
+            log "== BEGIN LOG ${logfile_basename} =="
+            cat "${logfile}"
+            log "== END LOG ${logfile_basename} =="
+            log
+        done
+
+        log
+        log "** END LOGS **"
+        log
+    fi
+
+    log "** BEGIN ACSMS PODS **"
+    $KUBECTL -n "$ACSMS_NAMESPACE" get pods || true
+    $KUBECTL -n "$ACSMS_NAMESPACE" describe pods || true
+    log "** END ACSMS PODS **"
+    log
+
+    log "** BEGIN OPERATOR STATE **"
+    $KUBECTL -n "$STACKROX_OPERATOR_NAMESPACE" get pods || true
+    $KUBECTL -n "$STACKROX_OPERATOR_NAMESPACE" describe pods || true
+    $KUBECTL -n "$STACKROX_OPERATOR_NAMESPACE" get subscriptions || true
+    $KUBECTL -n "$STACKROX_OPERATOR_NAMESPACE" describe subscriptions || true
+    $KUBECTL -n "$STACKROX_OPERATOR_NAMESPACE" get installplans || true
+    $KUBECTL -n "$STACKROX_OPERATOR_NAMESPACE" describe installplans || true
+    log "** END OPERATOR STATE **"
+    log
+
+    if [[ "$SPAWN_LOGGER" == "true" ]]; then
+        log "Logs are in ${LOG_DIR}"
+        log
+    fi
+fi
+
+log "=========="
+
+if [[ $FAIL == 0 ]]; then
+    log
+    log "** TESTS FINISHED SUCCESSFULLY **"
+    log
 else
-    log "Next: Executing e2e tests"
+    log
+    log "** TESTS FAILED **"
+    log
+fi
 
-    T0=$(date "+%s")
-    if ! make test/e2e; then
-        FAIL=1
-    fi
-    T1=$(date "+%s")
-    DELTA=$((T1 - T0))
-
-    if [[ $FAIL == 0 ]]; then
-        log
-        log "** E2E TESTS FINISHED SUCCESSFULLY ($DELTA seconds) **"
-        log
-    else
-        log
-        log "** E2E TESTS FAILED ($DELTA seconds) **"
-        log
-    fi
+if [[ "$FINAL_TEAR_DOWN" == "true" ]]; then
+    down.sh
+    delete "${MANIFESTS_DIR}/rhacs-operator" || true
 fi
 
 exit $FAIL
